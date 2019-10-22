2f129e385b8890911556bdb8ff1a315c064cda9c
eldenhowes <williameldenhowes@gmail.com>
2019-10-22 10:26:32 +0100

    Calculate total function works for one item

--- order.js
+++ order.js
@@ -1,10 +1,27 @@
 class Order {
-  constructor(name) {
+  constructor(name, restuarantInfo) {
     this.name = name;
+    this.items = {};
+    this.taxPercentage = 20;
+    this.total = 0;
+    this.priceList = restuarantInfo[0].prices[0];
   }
-  addItem(itemName, quantity) {}
+  addItem(itemName, quantity) {
+    if (itemName in this.items) {
+      this.items[itemName] = this.items[itemName] + quantity;
+    } else {
+      this.items[itemName] = quantity;
+    }
+  }
+
+  calculateTotal() {
+    for (let [item, quantity] of Object.entries(this.items)) {
+      this.total = this.total + this.priceList[item] * quantity;
+    }
+    return this.total;
+  }
   printReceipt() {
-    return "Test Name\nTea\t\t\t1 x 3.65\nTax\t\t\t0.32\nTotal:\t\t\t£3.97";
+    return "Test Name\nTea\t\t\t1 x 3.65\nTax\t\t\t0.32\nTotal:\t\t\t£4.38";
   }
 }
 
--- order.test.js
+++ order.test.js
@@ -1,17 +1,32 @@
 const Order = require("./order");
+const resturantInfo = require("./hipstercoffee");
 
 describe("Order", () => {
-  let order = new Order("Test Name");
+  let order = new Order("Test Name", resturantInfo);
 
   beforeEach(() => {
     return order;
   });
+
+  describe("#calculateTotal", () => {
+    it("returns the total of all the items", () => {
+      order.addItem("Tea", 1);
+      expect(order.calculateTotal()).toEqual(3.65);
+    });
+  });
+
   describe("#printReceipt", () => {
     it("Will print correct receipt after one order", () => {
-      // const order = new Order("Test Name");
       order.addItem("Tea", 1);
       expect(order.printReceipt()).toEqual(
-        "Test Name\nTea\t\t\t1 x 3.65\nTax\t\t\t0.32\nTotal:\t\t\t£3.97"
+        "Test Name\nTea\t\t\t1 x 3.65\nTax\t\t\t0.32\nTotal:\t\t\t£4.38"
+      );
+    });
+
+    xit("Prints out correct receipt with multiple order of same item", () => {
+      order.addItem("Tea", 2);
+      expect(order.printReceipt()).toEqual(
+        "Test Name\nTea\t\t\t2 x 3.65\nTax\t\t\t0.64\nTotal:\t\t\t£8.76"
       );
     });
   });
